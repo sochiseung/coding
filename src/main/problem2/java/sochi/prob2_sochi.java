726a22cd607af0bae77cbd978f58cb15f5ecab0d
sochiseung <sochiseung@gmail.com>
2017-05-31 19:03:21 +0900

    prob2_sochi

--- src/main/problem2/java/sochi/prob2_sochi.java
+++ src/main/problem2/java/sochi/prob2_sochi.java
@@ -13,4 +13,62 @@ package sochi;
  * @description :
  */
 public class prob2_sochi {
+
+    public static void main(String[] args){
+        LinkedList linkedList = new LinkedList(new int[]{1,2,3,4,5});
+        linkedList.findNthToEnd(1);
+    }
 }
+class LinkedList{
+
+
+    Node root;
+
+    LinkedList(int[] ints){
+        Node node = new Node(ints[0]);
+        root = node;
+        if(ints.length<=1) return;
+
+        for(int i=1; i<ints.length; i++){
+            Node addNode = new Node(ints[i]);
+            node.setNextNode(addNode);
+            node = addNode;
+        }
+
+    }
+    public Node getRoot() {
+        return root;
+    }
+    public void findNthToEnd(int n){
+        //n-1 skip
+        if(n < 0) return;
+        int count = 0;
+        Node node =root;
+        while(true){
+            if(count >= n) System.out.print(node.element);
+
+            if(node.getNextNode() == null) break;
+
+            node = node.getNextNode();
+            count++;
+        }
+    }
+}
+
+class Node{
+    Node tail = null;
+    int element;
+
+    Node(int element){
+        this.element = element;
+    }
+
+    Node getNextNode(){
+        return tail;
+    }
+    void setNextNode(Node node){
+        this.tail = node;
+    }
+}
+
+
