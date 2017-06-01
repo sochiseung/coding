8e7b7b8d7cef22f52841bc4634b999acac2613d0
sochiseung <sochiseung@gmail.com>
2017-06-01 23:38:29 +0900

    prob3

--- src/main/problem3/java/sochi/prob3_sochi.java
+++ src/main/problem3/java/sochi/prob3_sochi.java
@@ -1,5 +1,7 @@
 package sochi;
 
+import java.util.ArrayList;
+
 /**
  * @author : Intellicon Lab. soh
  * @version 1.0
@@ -13,4 +15,53 @@ package sochi;
  * @description :
  */
 public class prob3_sochi {
+
+    public static void main(String[] args){
+        Stack stack1 = new Stack();
+        stack1.Push(7);
+        stack1.Push(8);
+        stack1.Push(3);
+        stack1.Push(4);
+        stack1.Push(5);
+        stack1.Push(6);
+//        System.out.println(stack1.min());
+
+//        for(int i: stack1.arrayList){
+//            System.out.println(i);
+//        }
+
+        stack1.Pop();
+        for(int i: stack1.arrayList){
+            System.out.println(i);
+        }
+    }
+}
+
+class Stack{
+    private int min;
+    private int arraySize = 0;
+    public ArrayList<Integer> arrayList;
+
+    Stack(){
+        arrayList = new ArrayList<Integer>();
+    }
+    public void Push(int input){
+        if(arraySize ==0){
+            min = input;
+        }
+        else{
+            if(min > input) min = input;
+        }
+        arrayList.add(input);
+        arraySize++;
+    }
+    public int Pop(){
+        int result = arrayList.get(arraySize-1);
+        arrayList.remove(arraySize-1);
+        arraySize--;
+        return result;
+    }
+    public int min(){
+        return min;
+    }
 }
